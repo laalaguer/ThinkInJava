ac945148ef4281e86881390a5d6c82fa0337200b
Xiqing Chu <laalaguer@gmail.com>
2015-01-08 16:42:00 +0200

    09 lesson added

--- 09-composing-delegation-inheritance/Car.java
+++ 09-composing-delegation-inheritance/Car.java
@@ -24,6 +24,12 @@ public class Car{
 	// Let us make a car with 4 wheels, 1 engine and 1 controller.
 	// --> Composition
 	CarWheel[] cw = new CarWheel[4];
+	{
+		for (int i=0;i<cw.length;i++){
+			cw[i] = new CarWheel();
+		}
+
+	}
 	CarEngine ce = new CarEngine();
 	Controller controller = new Controller();
 	// But we still can tell car directly go forward and backward.
